77efdaa92617ef5ac7095eaf1c0b889287c55c99
Megha Sri Satya Sai Devineni <meghasri.devineni@gmail.com>
2023-11-28 16:46:40 -0800

    Updates

---
+++ precache-manifest.6637411bd1b98183ae19f9f69ba4352b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7f64624c27015449f1d5e3b448d6e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a66675d42be995d83b03",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ca6ebbd949cc97470bc6",
+    "url": "/portfolio/static/css/main.fd22dfbf.chunk.css"
+  },
+  {
+    "revision": "a66675d42be995d83b03",
+    "url": "/portfolio/static/js/2.1f731ba0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.1f731ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6ebbd949cc97470bc6",
+    "url": "/portfolio/static/js/main.2d4ea567.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "df906c59236eee87ea1d00a8bf1276a0",
+    "url": "/portfolio/static/media/Resume_Satya.df906c59.pdf"
+  },
+  {
+    "revision": "d150d4d60b034a7b8fc0df36dd389590",
+    "url": "/portfolio/static/media/github-circle.d150d4d6.svg"
+  },
+  {
+    "revision": "8fcac3b248cff064c0d887b34489cd24",
+    "url": "/portfolio/static/media/linkedin.8fcac3b2.svg"
+  },
+  {
+    "revision": "de6d90eb7797c70fa915af54e4ca6cf7",
+    "url": "/portfolio/static/media/profpic.de6d90eb.jpg"
+  },
+  {
+    "revision": "4b0253a7b65ea35f56ec5bc7ac437296",
+    "url": "/portfolio/static/media/shell.4b0253a7.png"
+  },
+  {
+    "revision": "cdfcfee93b03e3c02ce82f19c7a6c311",
+    "url": "/portfolio/static/media/uc.cdfcfee9.png"
+  },
+  {
+    "revision": "201c7aa9ea94e4e3c9e79fa22cf430e4",
+    "url": "/portfolio/static/media/vit.201c7aa9.png"
+  }
+]);
